6fc747b6cd4741844be2d659e2cd8a3d4c5bd90a
Lalit Adithya <lalitadithya5071@gmail.com>
2018-01-15 10:48:00 +0530

    Added implementation of IP address generator

---
+++ DataCenterManager/IPAddressGenerator.cs
@@ -0,0 +1,18 @@
+﻿using DataCenterManager.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using DataCenterManager.Data;
+
+namespace DataCenterManager
+{
+    class IPAddressGenerator : IIPAddressGenerator
+    {
+        public IPAddressSeries IPAddressSeries { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+
+        public IEnumerable<IPAddress> GetIPAddresses()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
