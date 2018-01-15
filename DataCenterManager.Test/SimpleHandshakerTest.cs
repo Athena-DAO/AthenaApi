2f8ad6a28ac60cf78f0adf09bf67d0dcef3a1d66
Lalit Adithya <lalitadithya5071@gmail.com>
2018-01-15 17:22:27 +0530

    Implemented optimistic handshake

---
+++ DataCenterManager.Test/SimpleHandshakerTest.cs
@@ -0,0 +1,76 @@
+﻿using DataCenterManager.Interfaces;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+
+namespace DataCenterManager.Test
+{
+    [TestClass]
+    public class SimpleHandshakerTest
+    {
+        private IHandshake _handshaker;
+
+        public SimpleHandshakerTest()
+        {
+            _handshaker = new SimpleHandshaker();
+        }
+
+        [TestMethod]
+        public void TestHandshake()
+        {
+            IPAddress iPAddress = IPAddress.Parse("127.0.0.1");
+            IPEndPoint localEndPoint = new IPEndPoint(iPAddress, 5000);
+            Socket socket = new Socket(iPAddress.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+            socket.Bind(localEndPoint);
+            socket.Listen(10);
+
+            new Thread(() =>
+            {
+                _handshaker.PerformHandshake(new Data.IPAddress
+                {
+                    FirstOctet = 127,
+                    SecondOctet = 0,
+                    ThridOctet = 0,
+                    FourthOctet = 1
+                });
+            }).Start();
+
+            Socket handler = socket.Accept();
+            byte[] bytes = new byte[255];
+            string data = null;
+            while(true)
+            {
+                int byteRecieved = handler.Receive(bytes);
+                data += Encoding.ASCII.GetString(bytes, 0, byteRecieved);
+                if(data.IndexOf("<EOF>") > -1)
+                {
+                    break;
+                }
+            }
+            data = data.Replace("<EOF>", "");
+            Assert.AreEqual("Hello", data);
+
+            byte[] numberOfContainers = Encoding.ASCII.GetBytes("2");
+            handler.Send(numberOfContainers);
+
+            bytes = new byte[255];
+            data = null;
+            while (true)
+            {
+                int byteRecieved = handler.Receive(bytes);
+                data += Encoding.ASCII.GetString(bytes, 0, byteRecieved);
+                if (data.IndexOf("<EOF>") > -1)
+                {
+                    break;
+                }
+            }
+            data = data.Replace("<EOF>", "");
+            Assert.AreEqual("Bye", data);
+
+            handler.Shutdown(SocketShutdown.Both);
+            handler.Close();
+        }
+    }
+}
--- DataCenterManager/SimpleHandshaker.cs
+++ DataCenterManager/SimpleHandshaker.cs
@@ -1,14 +1,48 @@
 ﻿using DataCenterManager.Interfaces;
-using System;
-using DataCenterManager.Data;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
 
 namespace DataCenterManager
 {
     public class SimpleHandshaker : IHandshake
     {
-        public void PerformHandshake(IPAddress IPAddress)
+        public void PerformHandshake(Data.IPAddress iPAddress)
         {
-            throw new NotImplementedException();
+            byte[] bytes = new byte[1024];
+            IPAddress myIPAddress = IPAddress.Parse(iPAddress.FirstOctet + "." + iPAddress.SecondOctet
+                + "." + iPAddress.ThridOctet + "." + iPAddress.FourthOctet);
+            IPEndPoint localEndPoint = new IPEndPoint(myIPAddress, 5000);
+            Socket socket = new Socket(myIPAddress.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+
+            PerformStageOne(localEndPoint, socket, out byte[] message, out int bytesSent);
+
+            int numberOfContainers = PerformStageTwo(bytes, socket);
+
+            PerformStageThree(socket, out message, out bytesSent);
+
+            socket.Shutdown(SocketShutdown.Send);
+            socket.Close();
+        }
+
+        private static void PerformStageThree(Socket socket, out byte[] message, out int bytesSent)
+        {
+            message = Encoding.ASCII.GetBytes("Bye<EOF>");
+            bytesSent = socket.Send(message);
+        }
+
+        private static int PerformStageTwo(byte[] bytes, Socket socket)
+        {
+            int bytesRecieved = socket.Receive(bytes);
+            int numberOfContainer = int.Parse(Encoding.ASCII.GetString(bytes, 0, bytesRecieved));
+            return numberOfContainer;
+        }
+
+        private static void PerformStageOne(IPEndPoint localEndPoint, Socket socket, out byte[] message, out int bytesSent)
+        {
+            socket.Connect(localEndPoint);
+            message = Encoding.ASCII.GetBytes("Hello<EOF>");
+            bytesSent = socket.Send(message);
         }
     }
 }
