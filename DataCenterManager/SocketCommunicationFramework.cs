fb59372547bc1934159442bcc453944e23e272fb
Lalit Adithya <lalitadithya5071@gmail.com>
2018-01-17 17:14:15 +0530

    Started implementation of socket based communcation framework.

---
+++ DataCenterManager/SocketCommunicationFramework.cs
@@ -0,0 +1,40 @@
+﻿using DataCenterManager.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataCenterManager
+{
+    class SocketCommunicationFramework : ICommunicationFramework
+    {
+        public void Close()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Connect(string IPAddress, int port)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string RecieveFile()
+        {
+            throw new NotImplementedException();
+        }
+
+        public string RecieveMessage()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SendFile(string fileLocation)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SendMessage(string data)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
