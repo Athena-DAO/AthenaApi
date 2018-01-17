72ff6504df0001faac57f715138339f2902352d3
Lalit Adithya <lalitadithya5071@gmail.com>
2018-01-17 17:12:32 +0530

    Added interface for communication framework

---
+++ DataCenterManager/Interfaces/ICommunicationFramework.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataCenterManager.Interfaces
+{
+    interface ICommunicationFramework
+    {
+        void Connect(string IPAddress, int port);
+        void SendMessage(string data);
+        string RecieveMessage();
+        void SendFile(string fileLocation);
+        string RecieveFile();
+        void Close();
+    }
+}
