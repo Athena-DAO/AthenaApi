402770822f798131d0919dc5d3896e8ea20c5f2b
Lalit Adithya <lalitadithya5071@gmail.com>
2018-01-20 20:55:58 +0530

    Added data authentication

--- CommandAndControlWebApi/ViewModels/RegistrationViewModel.cs
+++ CommandAndControlWebApi/ViewModels/RegistrationViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,8 +8,15 @@ namespace CommandAndControlWebApi.ViewModels
 {
     public class RegistrationViewModel
     {
+        [Required]
+        [EmailAddress]
+        [DataType(DataType.EmailAddress)]
         public string Email { get; set; }
+
+        [Required]
         public string Password { get; set; }
+
+        [Required]
         public string PhoneNumber { get; set; }
     }
 }
