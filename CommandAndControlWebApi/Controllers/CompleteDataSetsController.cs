bf10f7b6dcfaf7f10cc223e14dd5ac6f15247118
Lalit Adithya <lalitadithya5071@gmail.com>
2018-04-19 12:25:44 +0530

    Scaffolded controller for complete data sets

---
+++ CommandAndControlWebApi/Controllers/CompleteDataSetsController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+
+namespace CommandAndControlWebApi.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/CompleteDataSets")]
+    [Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme, Roles = "User")]
+    public class CompleteDataSetsController : Controller
+    {
+        // GET: api/CompleteDataSets
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET: api/CompleteDataSets/5
+        [HttpGet("{id}", Name = "Get")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+        
+        // POST: api/CompleteDataSets
+        [HttpPost]
+        public void Post([FromBody]string value)
+        {
+        }
+        
+        // PUT: api/CompleteDataSets/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+        
+        // DELETE: api/ApiWithActions/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
