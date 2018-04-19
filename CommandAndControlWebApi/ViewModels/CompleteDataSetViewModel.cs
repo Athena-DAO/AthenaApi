e84bfc46802b854af0e0153aa8bcbbac9bdbfa0d
Lalit Adithya <lalitadithya5071@gmail.com>
2018-04-19 12:23:36 +0530

    Added view model for complete data sets

---
+++ CommandAndControlWebApi/ViewModels/CompleteDataSetViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CommandAndControlWebApi.ViewModels
+{
+    public class CompleteDataSetViewModel
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string XComponentId { get; set; }
+        public string YComponentId { get; set; }
+    }
+}
