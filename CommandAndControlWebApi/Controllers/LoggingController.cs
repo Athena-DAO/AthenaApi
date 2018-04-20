ffa68d689084e101bd318be74b4742d3f3deacc9
Lalit Adithya <lalitadithya5071@gmail.com>
2018-04-20 20:58:06 +0530

    Added logging controller

---
+++ CommandAndControlWebApi/Controllers/LoggingController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using CommandAndControlWebApi.ViewModels;
+using System.IO;
+
+namespace CommandAndControlWebApi.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/Logging")]
+    public class LoggingController : Controller
+    {
+        // GET: api/Logging
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET: api/Logging/5
+        [HttpGet("{id}", Name = "GetLogs")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+        
+        // POST: api/Logging
+        [HttpPost]
+        public IActionResult Post([FromBody]PipelineLogViewModel value)
+        {
+            string targetFilePath = Path.Combine(Directory.GetCurrentDirectory(), "Logs", value.PipelineId) + ".txt";
+            System.IO.File.AppendAllText(targetFilePath, value.Log);
+            return Ok();
+        }
+        
+        // PUT: api/Logging/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+        
+        // DELETE: api/ApiWithActions/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ CommandAndControlWebApi/ViewModels/PipelineLogViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CommandAndControlWebApi.ViewModels
+{
+    public class PipelineLogViewModel
+    {
+        public string PipelineId { get; set; }
+        public string Log { get; set; }
+
+    }
+}
