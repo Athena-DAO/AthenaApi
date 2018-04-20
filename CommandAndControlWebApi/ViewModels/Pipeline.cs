cfa96476ef390a87a5c7bf487f2738500c5101d9
Lalit Adithya <lalitadithya5071@gmail.com>
2018-04-20 18:47:57 +0530

    Added view model for pipeline

---
+++ CommandAndControlWebApi/ViewModels/Pipeline.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CommandAndControlWebApi.ViewModels
+{
+    public class Pipeline
+    {
+        public string AlgorithmId { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public ICollection<PipelineParameter> Parameters { get; set; }
+    }
+}
---
+++ CommandAndControlWebApi/ViewModels/PipelineParameter.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CommandAndControlWebApi.ViewModels
+{
+    public class PipelineParameter
+    {
+        public string Id { get; set; }
+        public string Value { get; set; }
+    }
+}
